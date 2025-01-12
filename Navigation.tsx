fb474366e75ae448a1fbc170de3dbe101daf076d
nuzulasrar <80189595+nuzulasrar@users.noreply.github.com>
2025-01-12 19:42:49 +0800

    feat: setup for navigation done

---
+++ Navigation.tsx
@@ -0,0 +1,21 @@
+// In App.js in a new project
+
+import * as React from 'react'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './screens/Home'
+
+const Stack = createNativeStackNavigator()
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  )
+}
+
+export default function App() {
+  return <RootStack />
+}
